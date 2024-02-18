035147a43720e2c17822532c2b84f0b5f6be3ab6
alexliap <alexandrosliapates@gmail.com>
2024-02-18 19:33:24 +0200

    working state B.1 game

--- agent.py
+++ agent.py
@@ -7,7 +7,12 @@ from mapping import get_state_mapping
 
 class Agent:
     def __init__(
-        self, environment_size: tuple, possible_tile_states: tuple, actions: tuple
+        self,
+        environment_size: tuple,
+        possible_tile_states: tuple,
+        actions: tuple,
+        lr: float,
+        gamma: float,
     ):
         self.no_of_tiles = environment_size[0] * environment_size[1]
         # get state to row mapping
@@ -20,6 +25,8 @@ class Agent:
         self.q_table = self.init_q_table(len(self.state_row_mapping), self.action_space)
 
         self.epsilon = 1
+        self.lr = lr
+        self.gamma = gamma
 
     def init_q_table(self, state_space: int, action_space: int):
         return np.zeros((state_space, action_space))
@@ -28,7 +35,7 @@ class Agent:
         viable_actions = self.get_viable_actions(state)
         random_num = random.uniform(0, 1)
         if random_num > self.epsilon:
-            # get max available value on for a state
+            # get max available value for a state
             best_index = np.argmax(
                 self.q_table[self.state_row_mapping[str(state)]][viable_actions]
             )
@@ -53,5 +60,16 @@ class Agent:
 
         return viable_actions
 
-    def update_table(self):
-        pass
+    def update_table(self, state: tuple, action: dict, new_state: tuple, reward: int):
+        chosen_action = (action["tile"] * 2, (action["tile"] * 2) + 1)[
+            action["action"] - 1
+        ]
+        # Q(s,a) = Q(s,a) + lr * (R + gamma * maxQ(s+1) - Q(s,a))
+        former_q = self.q_table[self.state_row_mapping[str(state)]][chosen_action]
+        max_q_new_state = self.q_table[self.state_row_mapping[str(new_state)]].max()
+
+        updated_q = former_q + self.lr * (
+            reward + self.gamma * max_q_new_state - former_q
+        )
+        # update Q value
+        self.q_table[self.state_row_mapping[str(state)]][chosen_action] = updated_q
--- game.py
+++ game.py
@@ -4,20 +4,8 @@ from tqdm import tqdm
 from agent import Agent
 
 
-class GameBOne:
-    """
-    This Game class refers to the B.1 game rules, where agents play one after the other and
-    have knowledge of their other agents actions.
-    """
-
-    def __init__(
-        self,
-        environment_size: tuple,
-        possible_tile_states: tuple,
-        actions: tuple,
-        reward: int,
-        neg_reward: int = 0,
-    ):
+class BaseGame:
+    def __init__(self, environment_size: tuple, reward: int, neg_reward: int = 0):
         self.reward = reward
         self.neg_reward = neg_reward
 
@@ -25,45 +13,31 @@ class GameBOne:
 
         self.state = self.reset_state(self.environment_size)
 
-        self.get_state()
-
-        # create the 2 agents (it's self play so the 2nd agent is the same one)
-        self.agent_1 = Agent(environment_size, possible_tile_states, actions)
-
     def reset_state(self, environment_size: tuple):
-        return tuple(np.zeros((environment_size[0], environment_size[1])).flatten())
+        return tuple(
+            np.zeros((environment_size[0], environment_size[1])).flatten().astype(int)
+        )
 
-    def play(self, epochs):
-        exploration_end = int(0.8 * epochs)
-        linear_decay_rate = 100 / exploration_end
-        for i in tqdm(range(epochs), bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}"):
-            while self.check_board():
-                # get 1st agent's action
-                action_1 = self.agent_1.action(self.state)
-                # update state based on the above action
-                self.state = self.update_state(action_1)
-                # reward agent if the move was correct, otherwise penalize him
-                action_legitimacy = self.check_action(action_1)
-                reward = self.give_reward(action_legitimacy)
-                # update agent's Q table
-                self.agent_1.update_table(reward)
-                # in case first action caused the episode to end
-                if self.check_board():
-                    continue
-                # get 2nd agent's action
-                action_2 = self.agent_1.action(self.state)
-                # update state based on the above action
-                self.state = self.update_state(action_2)
-                # reward agent if the move was correct, otherwise penalize him
-                action_legitimacy = self.check_action(action_2)
-                reward = self.give_reward(action_legitimacy)
-                # update agent's Q table
-                self.agent_1.update_table(reward)
+    def check_board(self):
+        # check if all tiles are colored in order to end episode
+        if np.min(self.state) != 0:
+            return False
+        else:
+            return True
 
-            # update epslion for the agents
-            self.agent_1.epsilon -= linear_decay_rate
-            # reset the state
-            self.state = self.reset_state(self.environment_size)
+    def give_reward(self, action_legitimacy: bool):
+        if action_legitimacy:
+            return self.neg_reward
+        else:
+            return self.reward
+
+    def update_state(self, action: dict):
+        # turn state from tuple to list
+        state = list(self.state)
+        # update its value
+        state[action["tile"]] = action["action"]
+        # return it to tuple again
+        return tuple(state)
 
     def check_action(self, action: dict):
         # check if the action was correct or not
@@ -89,28 +63,83 @@ class GameBOne:
             neighbors = [7, 5]
 
         for item in neighbors:
-            if self.state[item] != action["action"]:
+            if self.state[item] == action["action"]:
                 wrong_action = True
+                break
 
         return wrong_action
 
-    def check_board(self):
-        # check if all tiles are colored in order to end episode
-        if np.max(self.state) != 0:
-            return True
-        else:
-            return False
 
-    def update_state(self, action: dict):
-        # turn state from tuple to list
-        state = list(self.state)
-        # update its value
-        state[action["tile"]] = action["action"]
-        # return it to tuple again
-        return tuple(state)
+class GameBOne(BaseGame):
+    """
+    This Game class refers to the B.1 game rules, where agents play one after the other and
+    have knowledge of their other agents actions.
+    """
 
-    def give_reward(self, action_legitimacy: bool):
-        if action_legitimacy:
-            return self.reward
-        else:
-            return self.neg_reward
+    def __init__(
+        self,
+        environment_size: tuple,
+        possible_tile_states: tuple,
+        actions: tuple,
+        lr: float,
+        gamma: float,
+        reward: int,
+        neg_reward: int = 0,
+    ):
+        super().__init__(environment_size, reward)
+
+        # create the 2 agents (it's self play so the 2nd agent is the same one)
+        self.agent_1 = Agent(environment_size, possible_tile_states, actions, lr, gamma)
+
+    def play(self, epochs):
+        exploration_end = int(0.8 * epochs)
+        linear_decay_rate = 1 / exploration_end
+        for i in tqdm(range(epochs), bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}"):
+            print(f"Epoch: {i}")
+            while self.check_board():
+                # get 1st agent's action
+                action_1 = self.agent_1.action(self.state)
+                # update state based on the above action
+                new_state = self.update_state(action_1)
+                # reward agent if the move was correct, otherwise penalize him
+                action_legitimacy = self.check_action(action_1)
+                reward = self.give_reward(action_legitimacy)
+
+                print(self.state, action_1, new_state, reward)
+                # update agent's Q table
+                self.agent_1.update_table(self.state, action_1, new_state, reward)
+                # in case first action caused the episode to end
+                self.state = new_state
+                if not self.check_board():
+                    break
+                # get 2nd agent's action
+                action_2 = self.agent_1.action(self.state)
+                # update state based on the above action
+                new_state = self.update_state(action_2)
+                # reward agent if the move was correct, otherwise penalize him
+                action_legitimacy = self.check_action(action_2)
+                reward = self.give_reward(action_legitimacy)
+                # update agent's Q table
+                self.agent_1.update_table(self.state, action_2, new_state, reward)
+
+                print(self.state, action_2, new_state, reward)
+
+                self.state = new_state
+
+            # update epsilon for the agents
+            self.agent_1.epsilon -= linear_decay_rate
+            # reset the state
+            self.state = self.reset_state(self.environment_size)
+
+
+game = GameBOne(
+    environment_size=(3, 3),
+    possible_tile_states=(0, 1, 2),
+    actions=(1, 2),
+    lr=1e-4,
+    gamma=0.95,
+    reward=1,
+    neg_reward=0,
+)
+
+game.play(2000)
